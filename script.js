daf6798760485b809899a20103ff8daf111f7662
JuanJoseAriasJaramillo <juanjoseariasj@hotmail.com>
2022-01-31 21:07:05 +0100

    correccion about, contacto form eliminado, button scrollup,  animaciones

--- script.js
+++ script.js
@@ -4,8 +4,18 @@ $(document).ready(function(){
             $('.navbar').addClass("sticky")
         }else{
             $('.navbar').removeClass("sticky")
+        }if(this.scroll > 500){
+            $('.scroll-up-btn').addClass("show")
+        }else{
+            $('.scroll-up-btn').removeClass("show")
+
         }
     });
+    //slide up script
+    $('.scroll-up-btn').click(function(){
+        $('html').animate({scrollTop: 0})
+    })
+
     //togle menu/navbar script
     $('.menu-btn').click(function(){
         $('.navbar .menu').toggleClass("active")
