d080ec434c2a4941ca9cbcdabcbb6ee12ec9fcd6
JuanJoseAriasJaramillo <juanjoseariasj@hotmail.com>
2022-01-31 21:37:51 +0100

    Arreglado el bug del boto scrollUp, y otra cosa que no recuerdo

--- script.js
+++ script.js
@@ -1,27 +1,25 @@
 $(document).ready(function(){
     $(window).scroll(function(){
+        // sticky navbar on scroll script
         if(this.scrollY > 20){
-            $('.navbar').addClass("sticky")
+            $('.navbar').addClass("sticky");
         }else{
-            $('.navbar').removeClass("sticky")
-        }
-        if(this.scroll > 500){
-            $('.scroll-up-btn').addClass("show")
+            $('.navbar').removeClass("sticky");
+        }        
+        if(this.scrollY > 500){
+            $('.scroll-up-btn').addClass("show");
         }else{
-            $('.scroll-up-btn').removeClass("show")
-
+            $('.scroll-up-btn').removeClass("show");
         }
     });
     //slide up script
     $('.scroll-up-btn').click(function(){
         $('html').animate({scrollTop: 0});
     });
-
     //togle menu/navbar script
     $('.menu-btn').click(function(){
         $('.navbar .menu').toggleClass("active")
         $('.menu-btn i').toggleClass("active")
-
     });
     //typing animation script
     var typed = new Typed(".typing", {
@@ -30,7 +28,7 @@ $(document).ready(function(){
         backSpeed: 60,
         loop:true
     });
-    var typed = new Typed(".typing-2", {
+    var typed = new Typed(".typing", {
         strings: ["Learner","Web Developer","RcPlaner"],
         typeSpeed: 100,
         backSpeed: 60,
