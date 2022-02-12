f9ab8062669f400bb10db39ee6505e8159bede50
JuanJoseAriasJaramillo <juanjoseariasj@hotmail.com>
2022-02-12 12:22:48 -0500

    versionFinal

--- script.js
+++ script.js
@@ -23,13 +23,13 @@ $(document).ready(function(){
     });
     //typing animation script
     var typed = new Typed(".typing", {
-        strings: ["Learner","Web Developer","RcPlaner"],
+        strings: ["Aprendiz","Desarrollador Web","Aeromodelista", "Ciclomontanista"],
         typeSpeed: 100,
         backSpeed: 60,
         loop:true
     });
     var typed = new Typed(".typing-2", {
-        strings: ["Learner","Web Developer","RcPlaner"],
+        strings: ["Aprendiz","Desarrollador Web","Aeromodelista", "Ciclomontanista"],
         typeSpeed: 100,
         backSpeed: 60,
         loop:true
